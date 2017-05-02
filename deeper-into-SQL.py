3c2b0583558c876a42bbd364b66938331d8428fd
Christiaan Quyn <christianq010@gmail.com>
2017-05-02 21:54:13 +0530

    feat: Add Query to create table alogn with declared primary keys

--- deeper-into-SQL.py
+++ deeper-into-SQL.py
@@ -9,3 +9,25 @@ CREATE_DATABASE_and_connect = {
     # or exit psql prompt and run `psql fishies`
 }
 
+CREATE_TABLE_AND_DECLARE_PRIMARY_KEYS = {
+    """
+    create table students (
+    id serial primary key,
+    name text,
+    birthday date
+    );
+    """
+    # an example insert into the above table 
+    """ insert into students values ('This is text!'); """
+    
+    # Multi column primary key
+    """
+    create table postal_places (
+    postal_code text,
+    country text,
+    name text,
+    primary key (postal_code, country)    
+    );
+    """
+
+}
