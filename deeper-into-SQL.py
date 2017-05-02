c086754fdda1dc87407b4e4c406e78ad9454f61c
Christiaan Quyn <christianq010@gmail.com>
2017-05-02 21:38:09 +0530

    feat: Add Query to create new database and connect to it

---
+++ deeper-into-SQL.py
@@ -0,0 +1,11 @@
+CREATE_DATABASE_and_connect = {
+    # In the VM enter the psql prompt
+
+    # create database name;
+    """ create database fishies;"""
+    
+    # to connect to the database
+    """\c fishies"""
+    # or exit psql prompt and run `psql fishies`
+}
+
