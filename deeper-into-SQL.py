94651c75f37d670a728b566a7eade3da3486febf
Christiaan Quyn <christianq010@gmail.com>
2017-05-03 12:26:10 +0530

    feat: Solution to Subquery Quiz

--- deeper-into-SQL.py
+++ deeper-into-SQL.py
@@ -166,4 +166,29 @@ Subqueries = {
     
     """
 
+# Find the players whose weight is less than the average.
+# 
+# The function below performs two database queries in order to find the right players.
+# Refactor this code so that it performs only one query.
+
+    # Returns a list of the players in the db whose weight is less than the average.
+    """
+    def lightweights(cursor):
+        cursor.execute("select avg(weight) as av from players;")
+        av = cursor.fetchall()[0][0]  # first column of first (and only) row
+        cursor.execute("select name, weight from players where weight < " + str(av))
+        return cursor.fetchall()
+    """    
+    # Solution
+
+    """
+    def lightweights(cursor):
+        cursor.execute("select name, weight
+                        from players,
+                            (select avg(weight) as av
+                                from players) as subq
+                        where weight < av;")
+        av = cursor.fetchall()[0][0]  # first column of first (and only) row
+        return cursor.fetchall()
+    """
 }
